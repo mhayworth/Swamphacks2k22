5ff38ded797860fe3c35ffdc3934310d207b5c57
mhayworth <73895101+mhayworth@users.noreply.github.com>
2022-01-29 17:26:38 -0500

    Finalized main parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,101 +1,37 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 
 #include "State.h"
 
 using namespace std;
 
-void interact() {
-    cout << "Welcome to [BOT_NAME]! Please type the number of your selection:" << endl;
-    cout << "1. Compare my job to average local salary" << endl;
-    cout << "2. Give me my estimated take home pay after costs of living" << endl;
-    cout << "3. Evaluate my job offer" << endl;
-
-    string userInput;
-    cin >> userInput;
-    while (true) {
-        try {
-            (stof(userInput));
-            break;
-        }
-        catch (...) {
-            cout << "Please enter just the number of your selection!" << endl;
-            cin >> userInput;
-        }
-    }
-    
-    cout << "Enter your current state's name:" << endl;
-    string homeState;
-    cin >> homeState;
-    State home = State(homeState);
-
-    cout << "Enter your post-tax annual salary as a number - no commas, please!" << endl;
-    string salary;
-    cin >> salary;
-
-    cout << "Do you live frugally, normally, or luxuriously? (hint: type the word exactly as it appears)" << endl;
-    string lifeStyle;
-    cin >> lifeStyle;
-    float lifeStyleMultiplier;
-
-    if (lifeStyle == "frugally") {
-        lifeStyleMultiplier = 0.75f;
-    }
-    else if (lifeStyle == "luxuriously") {
-        lifeStyleMultiplier == 1.5f;
-    }
-    else {
-        lifeStyleMultiplier = 1;
-    }
-
-    if (home.getLivingCost() == -1) {
-        cout << "Error: could not find state " << homeState << endl;
-        return;
-    }
-    int takeHomePay = stoi(salary) - (15000 * home.getLivingCost() / 100 * lifeStyleMultiplier);
-
-    switch (stoi(userInput)) {
-        case 1: {
-            vector<string> majorJobs = home.getJob().getMajorJobs();
-            cout << "Select a major job field" << endl;
-            for (int i = 0; i < majorJobs.size(); i++) {
-                cout << i << ". " << majorJobs[i] << endl;
-            }
-            // add more
-            break;
-        }
-        case 2: {
-            cout << "Your take home pay after considering costs of living and lifestyle is " << takeHomePay << endl;
-            break;
-        }
-        case 3: {
-            cout << "Your current take home pay is " << takeHomePay << endl;
-            break;
-        }
-    }
-    return;
-}
-
 void helpDialog() {
     cout << "Thank you for using the Comparing Salaries Bot!" << endl;
     cout << "Functionality is as follows:" << endl;
-    cout << "1. !SC is to compare your salary to a local average." << endl;
-    cout << "2. !SP is to find your take home pay after factoring in cost of living." << endl;
-    cout << "3. !SE is to evaluate a job offer." << endl;
-    cout << "For help with any of these commands, try !<name> help - ex. '!SC help'" << endl;
+    cout << "1. '!CS compare' is to compare your salary to a local average." << endl;
+    cout << "Functionality for take home pay comparison is: !SC <State_Name> <Salary> <Job_Title>" << endl;
+    cout << "<Salary> should have no commas and be pre-tax, and more information on <Job_Title> can be found through !SC help" << endl;
+    cout << "Example: '!CS compare Florida 75000 Aerospace-Engineers' would be your input if you live in Florida, make $75,000 pre-tax are an aerospace engineer" << endl;
+    cout << endl;
+    cout << "2. '!CS pay' is to find your take home pay after factoring in cost of living." << endl;
+    cout << "Functionality for take home pay is: !SP <State_Name> <Salary> <Lifestyle>" << endl;
+    cout << "<Salary> should have no commas and be pre-tax, while <Lifestyle> can be 'frugally', 'normally', 'luxuriously'" << endl;
+    cout << "Example: '!CS pay Florida 75000 frugally' would be your input if you live in Florida, make $75,000 pre-tax annually and live frgually" << endl;
+    cout << endl;
+    cout << "3. '!CS evaluate' is to evaluate a job offer." << endl;
+    cout << "Functionality for evaluation is: !CS evaluate <Current_State> <Current_Salary> <New_State> <New_Salary> <Lifestyle>. See 2. for lifestyle info" << endl;
+    
 }
 
 void takeHomeDialog(int argc, char* argv[]) {
-    if (argc < 4) {
-        cout << "Functionality for take home pay is: !SP <State_Name> <Salary> <Lifestyle>" << endl;
-        cout << "<Salary> should have no commas and be post-tax, while <Lifestyle> can be 'frugally', 'normally', 'luxuriously'" << endl;
-        cout << "Example: '!SP Florida 75000 frugally' would be your input if you live in Florida, make $75,000 post-tax annually and live frgually" << endl;
+    if (argc != 5) {
         return;
     }
-    string homeState = argv[1];
+    string homeState = argv[2];
     State home = State(homeState);
-    string salary = argv[2];
-    string lifeStyle = argv[3];
+    string salary = argv[3];
+    string lifeStyle = argv[4];
 
     float lifeStyleMultiplier;
     if (lifeStyle == "frugally") {
@@ -118,29 +54,17 @@ void takeHomeDialog(int argc, char* argv[]) {
     return;
 }
 
-void SChelp() {
-    cout << "Job titles can be split into major and minor jobs. Major jobs are:" << endl;
-    State home = State("Florida");
-    vector<string> majorJobs = home.getJob().getMajorJobs();
-}
 
 void compareDialog(int argc, char* argv[]) {
-    if (argc < 4) {
-        cout << "Functionality for take pay comparison is: !SC <State_Name> <Salary> <Job_Title>" << endl;
-        cout << "<Salary> should have no commas and be post-tax, and more information on <Job_Title> can be found through !SC help" << endl;
-        cout << "Example: '!SC Florida 75000 Aerospace-Engineers' would be your input if you live in Florida, make $75,000 post-tax are an aerospace engineer" << endl;
+    if (argc != 5) {
         return;
     }
-    if (argv[1] == "help") {
-        SChelp();
-        return;
-    }
-    string homeState = argv[1];
+    string homeState = argv[2];
     State home = State(homeState);
-    string salary = argv[2];
-    string jobTitle = argv[3];
+    string salary = argv[3];
+    string jobTitle = argv[4];
     int avgSalary = 0;//home.getJob().getSalary();
-    cout << "Your states average salary is $" << avgSalary;
+    cout << "Your state's average salary is $" << avgSalary;
     if (avgSalary > stoi(salary)) {
         cout << ", which is higher than your salary. You may want to look for better offers in the area." << endl;
     }
@@ -149,22 +73,55 @@ void compareDialog(int argc, char* argv[]) {
     }
 }
 
-void evaluateDialog() {
-    return;
+void evaluateDialog(int argc, char* argv[]) {
+    string homeState = argv[2];
+    State home = State(homeState);
+    string salary = argv[3];
+    string nState = argv[4];
+    State newState = State(nState);
+    string newSalary = argv[5];
+    string lifeStyle = argv[6];
+    float lifeStyleMultiplier;
+    if (lifeStyle == "frugally") {
+        lifeStyleMultiplier = 0.75f;
+    }
+    else if (lifeStyle == "luxuriously") {
+        lifeStyleMultiplier == 1.5f;
+    }
+    else {
+        lifeStyleMultiplier = 1;
+    }
+
+    if (home.getLivingCost() == -1) {
+        cout << "Error: could not find state " << homeState << endl;
+        return;
+    }
+    if (newState.getLivingCost() == -1) {
+        cout << "Error: could not find state " << nState << endl;
+    }
+    int takeHomePay = stoi(salary) - (15000 * home.getLivingCost() / 100 * lifeStyleMultiplier);
+    int newTakeHomePay = stoi(newSalary) - (15000 * newState.getLivingCost() / 100 * lifeStyleMultiplier);
+    if (newTakeHomePay > takeHomePay) {
+        cout << "Your new offer has a higher take home pay of " << newTakeHomePay << " versus " << takeHomePay << endl;
+    }
+    else {
+        cout << "Your current job has a higher take home pay of " << takeHomePay << " versus " << newTakeHomePay << endl;
+    }
+
 }
 
 int main(int argc, char* argv[]) {
-    if (argc == 0) {
+    if (argc < 2) {
         helpDialog();
     }
-    else if (argv[0] == "!SC") {
+    else if (strcmp(argv[1], "compare") == 0) {
         compareDialog(argc, argv);
     }
-    else if (argv[0] == "!SP") {
+    else if (strcmp(argv[1], "pay") == 0) {
         takeHomeDialog(argc, argv);
     }
-    else if (argv[0] == "!SE") {
-        evaluateDialog();
+    else if (strcmp(argv[1], "evaluate") == 0) {
+        evaluateDialog(argc, argv);
     }
     else {
         helpDialog();
